9ed5b28c47d4d2e05080a0660faab65a2f575b69
ggodreau <godreau@gmail.com>
2018-05-17 14:03:22 -0500

    5_27 added canvas and shape files

---
+++ Shape.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MethodOverriding
+{
+
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("draw a circle");
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("draw a rectangle");
+        }
+    }
+
+    public class Triangle: Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("draw a Triangle");
+        }
+    }
+
+    public class Shape
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public virtual void Draw()
+        {
+
+        }
+    }
+
+}
