03af7a1883f001d42a35115be9fe8dfab1bfa6d4
ggodreau <godreau@gmail.com>
2018-06-23 10:47:10 -0500

    6_35 halfway (ConsoleLogger complete), need implement FileLogger interface

---
+++ Extensibility/DbMigrator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Extensibility
+{
+    public class DbMigrator
+    {
+        private readonly ILogger _logger;
+
+        // constructor
+        public DbMigrator(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Migrate()
+        {
+            _logger.LogInfo("migration started at: " +  DateTime.Now);
+            // function to migrate the database goes here
+            _logger.LogInfo("migration finished at: " +  DateTime.Now);
+        }
+    }
+}
---
+++ Extensibility/ILogger.cs
@@ -0,0 +1,8 @@
+﻿namespace Extensibility
+{
+    public interface ILogger
+    {
+        void LogError(string messsage);
+        void LogInfo(string messsage);
+    }
+}
